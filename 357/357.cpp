3ce8d1e2079858fb7a973f992b204a6a3c441d29
Mansur <50706718+SlorrUndef@users.noreply.github.com>
2022-02-03 23:05:54 +0300

    Update 357.cpp

--- 357/357.cpp
+++ 357/357.cpp
@@ -1,1 +1,14 @@
-
+class Solution {
+public:
+    int factorial(int n){
+     return (n==0) || (n==1) ? 1 : n* factorial(n-1);
+}
+    int countNumbersWithUniqueDigits(int n) {
+        int arr[n+1];
+        arr[0] = 1;
+        for (int i = 1; i <= n; i++){
+            arr[i] = arr[i-1]+9*factorial(9)/factorial(10-i); // 9 variant to first digit, 9 variant to second digit, 8 for third, 7 for fourth and etc... 
+        }
+        return arr[n];
+    }
+};
