d1fb2d25371714a1a550f0260e7827c9cd2449ca
Mansur <50706718+SlorrUndef@users.noreply.github.com>
2022-02-16 00:46:17 +0300

    Time: 3 ms, Memory: 6.4 MB - LeetHub

---
+++ pascals-triangle/pascals-triangle.cpp
@@ -0,0 +1,21 @@
+/*TIME: O(rowIndex^2); MEMORY: O(rowIndex^2)
+  Algo: iteratively update the array from the end to the beginning.
+*/
+class Solution {
+public:
+    vector<int> getRow(int rowIndex) {
+        vector<int> res(rowIndex+1, 0);
+        res[0] = 1;
+        for(int i=1; i<rowIndex+1; i++)
+            for(int j=i; j>=1; j--)
+                res[j] += res[j-1];
+        return res;
+    }
+    vector<vector<int>> generate(int numRows) {
+        vector<vector<int>> res;
+        for(int i = 0; i < numRows; ++i){
+            res.push_back(getRow(i));
+        }
+        return res;
+    }
+};
