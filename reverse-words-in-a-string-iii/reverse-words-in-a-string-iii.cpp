4e411329bebd1dec12b5621f67dab7d99bac2324
Mansur <50706718+SlorrUndef@users.noreply.github.com>
2022-02-16 01:46:45 +0300

    Time: 7 ms, Memory: 9.5 MB - LeetHub

---
+++ reverse-words-in-a-string-iii/reverse-words-in-a-string-iii.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    
+    void reverseString(string &s,int i, int j){
+        for(int k=i; k<(j+i)/2; k++){
+            char c=s[k];
+            s[k]=s[j-k+i-1];
+            s[j-k+i-1]=c;
+        }
+    }
+    
+    string reverseWords(string s) {
+        int i = 0;
+        int j = 0;
+        int n = s.size();
+        while(i<n){
+            if(s[i] == ' ') {reverseString(s,j,i); j = i+1;}
+            if(i == n-1){reverseString(s,j,i+1);}
+            i++;
+        }
+        return s;
+    }
+};
