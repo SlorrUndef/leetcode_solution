4d8e769f4c75ae4795bd617d78a25c210e6d2597
Mansur <50706718+SlorrUndef@users.noreply.github.com>
2022-02-15 16:01:37 +0300

    Time: 384 ms (5.16%), Space: 18.4 MB (67.43%) - LeetHub

--- 498-diagonal-traverse/498-diagonal-traverse.cpp
+++ 498-diagonal-traverse/498-diagonal-traverse.cpp
@@ -1,5 +1,37 @@
+/*TIME: O(n*m); MEMORY: O(1)
+The first diagonal is (0, 0). The second is (0, 1), (1, 0), the third is (2, 0), (1, 1), (0, 2), etc.
+
+It should be clear that the sum of row i and column j is equal to the index of the diagonal (diagonal number - 1). e.g. for the second diagonal (index 1), all possible pairings of (i, j) sum to 1, i.e. i + j = 1 for the 2nd diagonal. The maximum diagonal index is simply ((N-1) + (M-1)) = N + M - 2
+
+So to solve the problem we simply need iterate through all possible diagonal indices (denote this as s) and find all possible pairs (i, j) such that i + j = s. The only thing we need to concern ourselves about is the order. We can find the ordering by looking at whether the diagonal index is even or odd. When the diagonal index is even we want to the first pair to be (s, 0) and when it is odd when want the first pair to be (0, s), and we decrease or increase i/j by 1 accordingly.
+*/
 class Solution {
 public:
+    vector<int> findDiagonalOrder(vector<vector<int>>& matrix) {
+        if(matrix.empty()) return {};
+        
+        const int N = matrix.size();
+        const int M = matrix[0].size();
+        
+        vector<int> res;
+        for(int s = 0; s <= N + M - 2; ++s)
+        {
+            // for all i + j = s
+            for(int x = 0; x <= s; ++x) 
+            {
+                int i = x;
+                int j = s - i;
+                if(s % 2 == 0) swap(i, j);
+
+                if(i >= N || j >= M) continue;
+                
+                res.push_back(matrix[i][j]);
+            }
+        }
+        
+        return res;
+    }
+    /* MY SOLUTION
     vector<int> findDiagonalOrder(vector<vector<int>>& mat) {
         int m = mat.size(), n = mat[0].size();
         vector <int> res;
@@ -36,5 +68,5 @@ public:
             k++;
         }
         return res;
-    }
+    } */
 };
