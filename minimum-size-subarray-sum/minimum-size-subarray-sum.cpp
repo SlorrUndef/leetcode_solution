a85dea2f49220195821b6f11c69037760bf40837
Mansur <50706718+SlorrUndef@users.noreply.github.com>
2022-02-16 01:42:47 +0300

    Time: 8 ms, Memory: 10.6 MB - LeetHub

--- minimum-size-subarray-sum/minimum-size-subarray-sum.cpp
+++ minimum-size-subarray-sum/minimum-size-subarray-sum.cpp
@@ -1,8 +1,13 @@
+/*TIME: O(1); MEMORY O(1)
+  Algo: sum elements from start until we get sum greater than or equal target;
+  Then remove elements from start of our sum, until we greater than or equal target;
+  and save length of sum; 
+*/
 class Solution {
 public:
     int minSubArrayLen(int target, vector<int>& nums) {
         int k = 0, p = INT_MAX;
-        int sum = 0, first = 0, last = 0, i = 0;
+        int sum = 0, first = 0, i = 0;
         while(i < nums.size()){
             if(sum < target){
                 if(k == 0) first = i; 
