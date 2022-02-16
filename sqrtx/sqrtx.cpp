785be95d908f5c2d4d340245bf477899ec5f9177
Mansur <50706718+SlorrUndef@users.noreply.github.com>
2022-02-16 21:37:55 +0300

    Time: 9 ms, Memory: 5.9 MB - LeetHub

--- sqrtx/sqrtx.cpp
+++ sqrtx/sqrtx.cpp
@@ -1,43 +1,21 @@
+/*TIME:O(logn)
+  Algo: binary search elements such that a*a == x
+  if we didn't found check if left*left > x return left-1 else return left
+*/
 class Solution {
 public:
-    int binarySearch(int n, int target){
-        if(n == 0)
-            return 0;
-        int mid;
-        int left = 0, right = n - 1;
-        while(left <= right){
-        // Prevent (left + right) overflow
-            mid = left + (right - left) / 2;
-            if (mid == 0){
-                left = mid + 1;
-                continue;
-            }
-            if(mid == target/mid){ return mid; }
-            else if(mid < target/mid) { left = mid + 1; }
-            else { right = mid - 1; }
-        }
-        if(mid < target/mid){
-            mid = ceil(mid);
-        } else{
-            mid = floor(mid);
-        }
-        // End Condition: left > right
-        return mid;
-    }
     int mySqrt(int x) {
         if(x == 0) return 0;
         if(x < 4) return 1;
         int mid, left = 0, right = x;
         while(left < right){
             mid = left + (right-left)/2;
-            if(mid == x/mid){       
-                return mid;
-            } else if(mid < x/mid){
+            if(mid <= x/mid){
                 left = mid+1;
             } else{
-                right = mid-1;
+                right = mid;
             }
         }
-        return (left*left > x ? left-1 : left);
+        return (left > x/left ? left-1 : left);
     }
 };
