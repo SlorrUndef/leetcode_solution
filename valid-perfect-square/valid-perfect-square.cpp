392c16468b2a11df82f8e63f1448bb7beabdb5bd
Mansur <50706718+SlorrUndef@users.noreply.github.com>
2022-02-16 22:46:28 +0300

    Time: 0 ms, Memory: 5.8 MB - LeetHub

---
+++ valid-perfect-square/valid-perfect-square.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    
+    bool isPerfectSquare(int num) {
+        if(num == 1) return true;
+        int mid, left = 0, right = num;
+        while(left < right){
+            mid = left + (right-left)/2;
+            if (mid <= num/mid) left = mid + 1;
+            else right = mid;
+        }
+        return ((left-1)*(left-1) == num ? true : false);
+    }
+};
