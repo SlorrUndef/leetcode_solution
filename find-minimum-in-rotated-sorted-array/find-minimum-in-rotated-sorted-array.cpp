02bbc72dc22636cafa4c6cd37c9b5dc4ce311179
Mansur <50706718+SlorrUndef@users.noreply.github.com>
2022-02-16 22:34:14 +0300

    Time: 0 ms, Memory: 10.1 MB - LeetHub

--- find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.cpp
+++ find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.cpp
@@ -1,3 +1,10 @@
+/*TIME: O(logn)
+  Algo: we have two sorted parts in vector; answer element will be element who was first in unrotated vector
+  if (nums[mid] < nums[0]) then mid and 0 in different part so we reducing right pointer
+  else we live in same part so we need to go in another part so we need to increase left pointer
+  CORNER CASE: if vector wasn't rotated nums[mid] > nums[0] for any mid; so left will be equal to nums.size()
+  and if so we can just return nums[0] as min element
+*/
 class Solution {
 public:
     int findMin(vector<int>& nums) {
