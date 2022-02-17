9b703cb8c757e401192dc8dd721690df5e6fb968
Mansur <50706718+SlorrUndef@users.noreply.github.com>
2022-02-18 01:29:16 +0300

    Time: 71 ms (50.45%), Space: 31.1 MB (88.05%) - LeetHub

--- 658-find-k-closest-elements/658-find-k-closest-elements.cpp
+++ 658-find-k-closest-elements/658-find-k-closest-elements.cpp
@@ -1,14 +1,17 @@
-/**/
+/*TIME: O(log(n-k) + k); MEMORY: O(k)
+  Algo: we need to find an interval such that the distance from x to the center of the interval is minimal and x lies to     the right or in the center of the interval. We can do it with binary search: O(log(n-k)); then we need to push k elements   to res vector : O(k)
+*/
 class Solution {
 public:
     vector<int> findClosestElements(vector<int>& arr, int k, int x) {
         int left = 0, right = arr.size() - k;
         while(left < right){
             int mid = left + (right-left)/2;
-            if(2*x > arr[mid+k] + arr[mid]){
-                left = mid + 1;
+            // x > (arr[mid+k]+arr[mid])/2; (arr[mid+k]+arr[mid])/2 - center of interval 
+            if(2*x > arr[mid+k] + arr[mid]){ 
+                left = mid + 1; //move interval to the right (to minimize distance)
             }else{
-                right = mid;
+                right = mid; //move interval to the left (to make x lie to the right or in the center of the interval)
             }
         }
         vector<int> res = {arr.begin() + left, arr.begin() + left + k};
