f42167c3fb5deb3bceb13245b19061bdd9267ce2
Mansur <50706718+SlorrUndef@users.noreply.github.com>
2022-02-19 04:04:57 +0300

    Time: 4 ms, Memory: 7 MB - LeetHub

---
+++ spiral-matrix/spiral-matrix.cpp
@@ -0,0 +1,43 @@
+/*Algo: there is 4 direction: 
+  1. go right ->
+  2. go bottom 
+  3. go left
+  4. go top
+  we need to put elements step by step in this directions right, bottom, left, top, right, bottom, left, top...
+  */
+class Solution {
+public:
+    vector<int> spiralOrder(vector<vector<int>>& matrix) {
+        vector<int> res;
+        int n=matrix.size();
+        int m=matrix[0].size();
+        int left=0, right=m-1, bottom=n-1, top=0;
+        int direction=1;
+        while(left <= right && top <= bottom){
+            if(direction == 1){
+                for(int i = left;i<=right; ++i) res.push_back(matrix[top][i]);
+                direction=2;
+                top++;
+            }
+            
+            else if(direction==2){
+                for(int i = top; i<=bottom; ++i) res.push_back(matrix[i][right]);
+                direction=3;
+                right--;
+            }
+            
+            else if(direction==3){
+                for(int i = right; i>=left; i--) res.push_back(matrix[bottom][i]);
+                direction=4;
+                bottom--;
+            }
+            
+            else if(direction==4){
+                for(int i = bottom; i>=top;i--) res.push_back(matrix[i][left]);
+                direction=1;
+                left++;
+            }
+        }
+        return res;
+    }
+};
