7728b6737f7afd751268120cd42f765de022bacc
Mansur <50706718+SlorrUndef@users.noreply.github.com>
2022-05-04 14:35:28 +0300

    Time: 104 ms, Memory: 36.9 MB - LeetHub

--- minimum-index-sum-of-two-lists/minimum-index-sum-of-two-lists.cpp
+++ minimum-index-sum-of-two-lists/minimum-index-sum-of-two-lists.cpp
@@ -1,27 +1,42 @@
 class Solution {
 public:
     vector<string> findRestaurant(vector<string>& list1, vector<string>& list2) {
-        vector<string> res;
-        unordered_map <string, int> hash;
-        int k = INT_MAX, t = 0;
-        for(int i = 0; i < list1.size(); ++i){
-            hash[list1[i]] = i;
-        }
-        for(int i = 0; i < list2.size(); ++i){
-            if(hash.count(list2[i]) > 0) {
-                hash[list2[i]] += i;
-                if(k > hash[list2[i]]){
-                    k = hash[list2[i]];
-                }
-            }
-        }
-        for(int i = 0; i < list2.size(); ++i){
-            if(hash.count(list2[i]) > 0) {
-                if(hash[list2[i]] == k){
+        vector<string>res;
+        unordered_map<string,int>hash;
+        int min = INT_MAX;
+        for(int i = 0; i < list1.size(); i++) hash[list1[i]] = i;
+        for(int i = 0; i < list2.size(); i++)
+            if(hash.count(list2[i]) != 0)
+                if(min > hash[list2[i]] + i) {
+                    min = hash[list2[i]] + i;
+                    res.clear();
                     res.push_back(list2[i]);
                 }
-            }
-        }
+                else if(hash[list2[i]] + i == min) res.push_back(list2[i]);
         return res;
     }
+    // vector<string> findRestaurant(vector<string>& list1, vector<string>& list2) {
+    //     vector<string> res;
+    //     unordered_map <string, int> hash;
+    //     int k = INT_MAX, t = 0;
+    //     for(int i = 0; i < list1.size(); ++i){
+    //         hash[list1[i]] = i;
+    //     }
+    //     for(int i = 0; i < list2.size(); ++i){
+    //         if(hash.count(list2[i]) > 0) {
+    //             hash[list2[i]] += i;
+    //             if(k > hash[list2[i]]){
+    //                 k = hash[list2[i]];
+    //             }
+    //         }
+    //     }
+    //     for(int i = 0; i < list2.size(); ++i){
+    //         if(hash.count(list2[i]) > 0) {
+    //             if(hash[list2[i]] == k){
+    //                 res.push_back(list2[i]);
+    //             }
+    //         }
+    //     }
+    //     return res;
+    // }
 };
