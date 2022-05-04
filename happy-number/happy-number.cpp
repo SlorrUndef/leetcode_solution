9628220bd934e3757a93a7a818ef850dcf099627
Mansur <50706718+SlorrUndef@users.noreply.github.com>
2022-05-04 11:22:14 +0300

    Time: 4 ms, Memory: 6.3 MB - LeetHub

--- happy-number/happy-number.cpp
+++ happy-number/happy-number.cpp
@@ -1,10 +1,16 @@
+/*
+Do summation of square of each digit and store the sum in hashset.
+Repeat step 1 until sum becomes 1 and every time check if value exists in hashset or not.
+If exists (we are in loop) then return false otherwise continue.
+*/
 class Solution {
 public:
     bool isHappy(int n) {
         unordered_set <int> res;
         int sum = n;
+
         while(sum != 1){
-            if(res.find(sum) != res.end()) return false;
+            if(res.find(sum) != res.end()) return false; //
             res.insert(sum);
             
             sum = 0;
